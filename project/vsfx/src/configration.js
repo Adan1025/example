5b46aa19fc581cb1ca4988f6774b5ddf3c6c172c
quancongjin550 <quancongjin55@pingan.com.cn>
2018-12-19 16:46:35 +0800

    upload

--- project/vsfx/src/configration.js
+++ project/vsfx/src/configration.js
@@ -4,9 +4,9 @@ module.exports = {
   // mysql配置
   mysql: {
     type: "mysql",
-    host: "sh-cdb-ni6jbci3.sql.tencentcdb.com",
-    username: "data",
-    password: "hi!xkayd@2018#",
+    host: "127.0.0.1",
+    username: "root",
+    password: "",
     database: "sf",
     port: "3306",
     acquireTimeout: "60 * 6 * 24",
