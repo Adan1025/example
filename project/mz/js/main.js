c3b037131ebd75831a7a7d0c813924ae3efa8078
qualc <493549174@qq.com>
2018-12-25 17:41:20 +0800

    tt

--- project/mz/js/main.js
+++ project/mz/js/main.js
@@ -1,6 +1,6 @@
-$(function () {
+$(function() {
     //机场服务tab切换
-    $(document).on('click', '.air-service-list', function () {
+    $(document).on('click', '.air-service-list', function() {
         var item = $(this).index();
         $('.air-service-list').removeClass('curr');
         $(this).addClass('curr');
@@ -8,5 +8,31 @@ $(function () {
             .show()
             .siblings('.air-service-details')
             .hide();
+    });
+    //根据默认加载时屏幕宽度来控制菜单显示隐藏
+    var screenW = $(window).width();
+    if (screenW > 768) {
+        $('.pc-air-service').show();
+        $('.m-air-service').hide();
+    } else {
+        $('.m-air-service').show();
+        $('.pc-air-service').hide();
+    }
+
+    //实时监听屏幕大小
+    $(window).resize(function() {
+        var screenW = $(window).width();
+        if (screenW > 767) {
+            $('.pc-air-service').show();
+            $('.m-air-service').hide();
+        } else {
+            $('.m-air-service').show();
+            $('.pc-air-service').hide();
+        }
+    });
+
+    // moblie 下拉菜单
+    $('.m-air-servicetab').on('click', '#mAirSerTab', function() {
+        $('.caret').toggleClass('m-transform');
     });
 });
